0d1ce908deef25866a233c07245e040e49d0494f
ParkerBacall <pbacall2@gmail.com>
2020-03-30 12:01:29 -0600

    emit when user enters and leaves chat

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketio = require('socket.io')
 
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
 //set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+//run when a client connects
+io.on('connection', socket => {
+    console.log('New WS connection')
+
+    socket.emit('message', "welcome to ChatCord!")
+
+    //Broadcast when a user connects
+    socket.broadcast.emit('message', 'A user has joined the chat')
+
+    //runs when client disconnects
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left the chat')
+    })
+})
+
 const PORT =  3000 || process.env.PORT;
 
-app.listen(PORT, () => console.log(`Server running on ${PORT}`))
+server.listen(PORT, () => console.log(`Server running on ${PORT}`))
