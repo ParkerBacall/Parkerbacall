e7a44752b781f749b4c13d3b53d66f43e958a816
ParkerBacall <pbacall2@gmail.com>
2020-03-30 12:56:03 -0600

    append messages to the dom

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,30 @@
+const chatForm = document.getElementById('chat-form')
+const chatMessages = document.querySelector('.chat-messages')
+
 const socket = io();
 
 
 socket.on('message', message => {
-    console.log(message)
+    outputMessage(message)
+
+    //scroll down
+    chatMessages.scrollTop = chatMessages.scrollHeight
 })
+
+chatForm.addEventListener('submit', event =>{
+    event.preventDefault()
+
+    const msg = event.target.elements.msg.value
+    
+    socket.emit('chatMessage', msg)
+})
+
+function outputMessage(message){
+    const div = document.createElement('div')
+    div.classList.add('message')
+    div.innerHTML = `<p class="meta">Mary <span>9:15pm</span></p>
+    <p class="text">
+        ${message}
+    </p>`
+    document.querySelector('.chat-messages').appendChild(div)
+}
