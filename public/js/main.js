c154005e39dfc88b6ceec783902db7b120b79ddb
ParkerBacall <pbacall2@gmail.com>
2020-03-31 12:06:20 -0600

    add timestamp functionality

--- public/js/main.js
+++ public/js/main.js
@@ -17,14 +17,19 @@ chatForm.addEventListener('submit', event =>{
     const msg = event.target.elements.msg.value
     
     socket.emit('chatMessage', msg)
+
+
+    event.target.elements.msg.value = ''
+    event.target.elements.msg.focus() = ''
+
 })
 
 function outputMessage(message){
     const div = document.createElement('div')
     div.classList.add('message')
-    div.innerHTML = `<p class="meta">Mary <span>9:15pm</span></p>
+    div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p>
     <p class="text">
-        ${message}
+        ${message.text}
     </p>`
     document.querySelector('.chat-messages').appendChild(div)
 }
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
+const formatMessage = require('./utils/messages')
 
 const app = express()
 const server = http.createServer(app)
@@ -10,21 +11,21 @@ const io = socketio(server)
 //set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+botName = "Chatcord Bot"
+
 //run when a client connects
 io.on('connection', socket => {
-    console.log('New WS connection')
-
-    socket.emit('message', "welcome to ChatCord!")
+    socket.emit('message', formatMessage(botName, "welcome to ChatCord!"))
 
     //Broadcast when a user connects
-    socket.broadcast.emit('message', 'A user has joined the chat')
+    socket.broadcast.emit('message', formatMessage(botName, 'A user has joined the chat'))
 
     //runs when client disconnects
     socket.on('disconnect', () => {
-        io.emit('message', 'A user has left the chat')
+        io.emit('message', formatMessage(botName, 'A user has left the chat'))
     })
     socket.on('chatMessage', msg => {
-        io.emit('message', msg)
+        io.emit('message',formatMessage('USER', msg))
     })
 })
 
